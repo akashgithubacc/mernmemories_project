7864881e20c94ce0d0558fa1c8d3309047ec2822
akashgithubacc <rohansingh25december@gmail.com>
2023-06-23 22:57:49 +0530

    index updated server

--- server/index.js
+++ server/index.js
@@ -27,20 +27,23 @@ const PORT = process.env.PORT || 5000;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose
-      .connect(process.env.CONNECTION_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() =>
-        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-      )
-      .catch((error) => console.log(error.message));
+    const conn = await mongoose.connect(process.env.CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
     process.exit(1);
   }
 };
 
-console.log("Before connecting to the database"); // Add this line
-connectDB();
+app.all("*", (req, res) => {
+  res.json({ "every thing": "is awesome" });
+});
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests");
+  });
+});
